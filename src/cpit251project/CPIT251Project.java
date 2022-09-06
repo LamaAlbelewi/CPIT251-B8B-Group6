059cc75f42fe564c89a3ec8874418494b02b4c38
Lama Albelewi <lama.m.albelewi@hotmail.com>
2022-09-06 11:36:55 +0300

    Hello

--- src/cpit251project/CPIT251Project.java
+++ src/cpit251project/CPIT251Project.java
@@ -15,7 +15,7 @@ public class CPIT251Project {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Hello I am Lama Albelewi :)");
     }
     
 }
